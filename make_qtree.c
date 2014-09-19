e3a0675ef147768734a97a694b34c08eb1645988
lukelev07 <lukelev07@gmail.com>
2014-09-19 00:32:41 -0700

    homogenous method implemented

--- make_qtree.c
+++ make_qtree.c
@@ -11,19 +11,29 @@
 #include "utils.h"
 
 #define ABS(x) (((x) < 0) ? (-(x)) : (x))
+#define NON_LEAF 256
 
 int homogenous(unsigned char *depth_map, int map_width, int x, int y, int section_width) {
+    // declare range of iteration through memory
+    int range = section_width * section_width;
+    int start = x + (y * map_width);
+    int hue = depth_map[start];
 
-    /* YOUR CODE HERE */
-    return 0;
-
+    // begin search for same val
+    for (i = start; i < range - 1; i++) {
+        if (!(depth_map[i] == hue)) {
+            return NON_LEAF;
+        }
+    }
+    // they must all be the same return the original hue
+    return hue;
 }
 
 qNode *depth_to_quad(unsigned char *depth_map, int map_width) {
-
-    /* YOUR CODE HERE */
-    return NULL;
-
+    // paranoid edge case 
+    if (!*depth_map) {
+        return NULL;
+    }
 }
 
 void free_qtree(qNode *qtree_node) {
