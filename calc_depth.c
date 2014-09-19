dd399e42a8b33bdb527726a6b7937aa3c4b93c83
James Carr <jcarr@berkeley.edu>
2014-09-19 02:30:18 -0700

    has bugs but compiles.  Added test images

--- calc_depth.c
+++ calc_depth.c
@@ -26,63 +26,84 @@ void calc_depth(unsigned char *depth_map, unsigned char *left,
 
     /* YOUR CODE HERE */
     //loop through each pixel in left
-    if (feature_hight > maximum_displacement || feature_width > maximum_displacement) {
+    if (feature_height > maximum_displacement || feature_width > maximum_displacement) {
     	printf("feature height larger than displacement box\n");
     }
 
     // calculate parameters to build feature
     //calculate parameters to build displacement box on right image
-    int box_dim = 2(maximum_displacement) + 1;
+    //int box_dim = 2*(maximum_displacement) + 1;
 
     int current_x;
     int current_y;
     int x;
+    int x2;
     int y;
+    int y2;
     int factor_x;
     int factor_y;
     int euc_dist_min;
     int euc_index;
+    int euc_pixel;
+    //located closest match
+    int best_x;
+    int best_y;
 
-    int i=feature_width+image_width*feature_height;
-    int j=(image_width*image_height-1)-image_width*feature_height-feature_width;
-	while (i <= j) {
+    //int i=feature_width+image_width*feature_height;
+    //int j=(image_width*image_height-1)-image_width*feature_height-feature_width;
+    int i = 0;
+	while (i != '\0') {
 		//start comparing with other factors inside the box on the right image
-		if (i == '\0') {
-			printf("wat, got to end\n");
-		}
+
 		current_x = i % image_width;
 		current_y = (i-current_x)/image_width;
 		
 		//x+y*width=i
-
-		if (current_x-feature_width-maximum_displacement < 0) {
+		
+		//TODO maybe remove first half or OR?
+		if (current_x-maximum_displacement < 0 || current_x-feature_width < 0) {
 			//big green box sticks over left edge
-			x=(maximum_displacement)
+			x=feature_width+1;
 		}
-		if (current_x+feature_width+maximum_displacement > image_width-1) {
+		if (current_x+maximum_displacement > image_width-1 || current_x+feature_width > image_width-1) {
 			//green box sticks over right edge
+			x2=feature_width+image_width;
 		}
-		if (current_y-feature_width-maximum_displacement < 0) {
+		if (current_y-maximum_displacement < 0 || current_y-feature_height < 0) {
 			//green box above top edge
+			y=feature_height+1;
 		}
-		if (current_y+feature_height+maximum_displacement > image_height -1) {
+		if (current_y+maximum_displacement > image_height -1 || current_y+feature_height > image_height -1) {
 			//green box below bottom edge
+			y2=feature_height+image_height;
 		}
 
 		//iterate on y from y=current_y+half_feature to current_y+max_disp-half_feature
 		//iterate on x from x=current_x+half_feature to current_x + max_disp - half_feature
 		else {
-			x=current_x+h_factor_neg-(box_dim-1)/2;
-			y=current_y+v_factor_neg-(box_dim-1)/2;
+			//x=current_x-maximum_displacement;
+			//x2=current_x+maximum_displacement;
+			//y=current_y+v_factor_neg-(box_dim-1)/2;
+		
+			y=current_y;
+			x=current_x;
 		}
-		while (y < current_y-v_factor_pos+(box_dim-1)/2){
-			while (x < current_x-h_factor_pos+(box_dim-1)/2) {
+
+		//TODO just determine how far right and how far down the box needs to go from the padded x calculated up top. then no need for actually figuring out the size of the giant green box.
+		while (y < 2*(maximum_displacement)+1-feature_height){
+			if (y>y2) {
+				printf("probably shouldn't happen y\n");
+			}
+			while (x < 2*(maximum_displacement)+1-feature_width) {
+				if (x>x2) {
+					printf("probably shouldn't happen x\n");
+				}
 				//iterate through factor and contribute these values to the euclidian distance
-				factor_x = current_x;
-				factor_y = current_y;
+				factor_x = current_x-feature_width;
+				factor_y = current_y-feature_width;
 				
-				while (factor_y < y+v_factor_pos) {
-					while (factor_x < x+h_factor_pos) {
+				while (factor_y < current_y+feature_width) {
+					while (factor_x < current_x+feature_width) {
 						euc_pixel = factor_x+factor_y*image_width;
 						euc_index += (left[euc_pixel]-right[euc_pixel])*(left[euc_pixel]-right[euc_pixel]);
 						factor_x++;
@@ -91,6 +112,8 @@ void calc_depth(unsigned char *depth_map, unsigned char *left,
 				}
 				if (euc_index < euc_dist_min) {
 					euc_dist_min=euc_index;
+					best_x=x;
+					best_y=y;
 				}
 				x++;
 			}
@@ -99,6 +122,7 @@ void calc_depth(unsigned char *depth_map, unsigned char *left,
 		i++;
 	}
 }
+
 
 // function idea
 //returns array of x-displacements to form the feature box around on the right image
