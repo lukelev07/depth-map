491e227495617071b71e76f3c2a1c345fe3d9654
lukelev07 <lukelev07@gmail.com>
2014-09-18 20:05:53 -0700

    First commit

---
+++ calc_depth.c
@@ -0,0 +1,30 @@
+/*
+ * PROJ1-1: YOUR TASK A CODE HERE
+ *
+ * Feel free to define additional helper functions.
+ */
+
+#include "calc_depth.h"
+#include "utils.h"
+#include <math.h>
+#include <limits.h>
+#include <stdio.h>
+
+/* Implements the normalized displacement function */
+unsigned char normalized_displacement(int dx, int dy,
+        int maximum_displacement) {
+
+    double squared_displacement = dx * dx + dy * dy;
+    double normalized_displacement = round(255 * sqrt(squared_displacement) / sqrt(2 * maximum_displacement * maximum_displacement));
+    return (unsigned char) normalized_displacement;
+
+}
+
+void calc_depth(unsigned char *depth_map, unsigned char *left,
+        unsigned char *right, int image_width, int image_height,
+        int feature_width, int feature_height, int maximum_displacement) {
+
+    /* YOUR CODE HERE */
+
+}
+
---
+++ make_qtree.c
@@ -0,0 +1,39 @@
+/*
+ * PROJ1-1: YOUR TASK B CODE HERE
+ *
+ * Feel free to define additional helper functions.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "quadtree.h"
+#include "make_qtree.h"
+#include "utils.h"
+
+#define ABS(x) (((x) < 0) ? (-(x)) : (x))
+
+int homogenous(unsigned char *depth_map, int map_width, int x, int y, int section_width) {
+
+    /* YOUR CODE HERE */
+    return 0;
+
+}
+
+qNode *depth_to_quad(unsigned char *depth_map, int map_width) {
+
+    /* YOUR CODE HERE */
+    return NULL;
+
+}
+
+void free_qtree(qNode *qtree_node) {
+    if(qtree_node) {
+        if(!qtree_node->leaf){
+            free_qtree(qtree_node->child_NW);
+            free_qtree(qtree_node->child_NE);
+            free_qtree(qtree_node->child_SE);
+            free_qtree(qtree_node->child_SW);
+        }
+        free(qtree_node);
+    }
+}
