98a2239c2b62abc714b6093682051e1c23fff373
lukelev07 <lukelev07@gmail.com>
2014-09-20 17:42:59 -0700

    Implemented buggy quad_help

--- make_qtree.c
+++ make_qtree.c
@@ -20,7 +20,7 @@ int homogenous(unsigned char *depth_map, int map_width, int x, int y, int sectio
     int hue = depth_map[start];
 
     // begin search for same val
-    for (i = start; i < range - 1; i++) {
+    for (int i = start; i < range - 1; i++) {
         if (!(depth_map[i] == hue)) {
             return NON_LEAF;
         }
@@ -29,16 +29,47 @@ int homogenous(unsigned char *depth_map, int map_width, int x, int y, int sectio
     return hue;
 }
 
+qNode *quad_help(unsigned char *quadrant, int quad_width, int x, int y) {
+    // recursively creates qNodes and assigns pointers.
+    if (homogenous(quadrant, quad_width, x, y, quad_width) != NON_LEAF) {
+        // base case assign leaf node and NULL children pointers
+        qNode *leaf = (qNode *)malloc(sizeof(qNode));
+        // set traits of LEAF
+        leaf->leaf = 1;
+        leaf->size = (quad_width * quad_width);
+        leaf->x = x;
+        leaf->y = y;
+        leaf->gray_value = homogenous(quadrant, quad_width, x, y, quad_width);
+        (*leaf).child_NW = NULL;
+        (*leaf).child_NE = NULL;
+        (*leaf).child_SE = NULL;
+        (*leaf).child_SW = NULL;
+        return leaf;
+    } else {
+        qNode *branch = (qNode *)malloc(sizeof(qNode));
+        // set traits of NON_LEAF
+        branch->leaf = 0;
+        branch->size = (quad_width * quad_width);
+        branch->x = x;
+        branch->y = y;
+        branch->gray_value = NON_LEAF;
+        (*branch).child_NW = quad_help(quadrant, quad_width / 2, 0, 0);
+        (*branch).child_NE = quad_help(quadrant, quad_width / 2, quad_width / 2, 0);
+        (*branch).child_SE = quad_help(quadrant, quad_width / 2, quad_width / 2, quad_width / 2);
+        (*branch).child_SW = quad_help(quadrant, quad_width / 2, 0, quad_width / 2);
+        return branch;
+    }
+}
+
 qNode *depth_to_quad(unsigned char *depth_map, int map_width) {
     // paranoid edge case 
     if (!*depth_map) {
         return NULL;
     } 
     // base case call helper
-    
-    // start at root check if homgenous
-    
+    return quad_help(depth_map, map_width, 0, 0);
 }
+
 // DO NOT MODIFY
 void free_qtree(qNode *qtree_node) {
     if(qtree_node) {
