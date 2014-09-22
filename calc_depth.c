2f98f7aa393d6ddb6b8edb45dbf4daca78801a34
James Carr <jcarr@berkeley.edu>
2014-09-21 17:44:13 -0700

    one-line variable initializations

--- calc_depth.c
+++ calc_depth.c
@@ -33,37 +33,15 @@ void calc_depth(unsigned char *depth_map, unsigned char *left,
     //calculate parameters to build displacement box on right image
     //int box_dim = 2*(maximum_displacement) + 1;
 
-    int current_x;
-    int current_y;
-    int x=0;
-    int xt=0;
-    int x2=0;
-    int y=0;
-    int y2=0;
-    int factor_x;
-    int factor_y;
-    int euc_dist_min=-1;
-    int euc_index=0;
-    int euc_pixel;
-    int left_pixel;
-    int euc_me=0;
-    //located closest match
-    int best_x;
-    int best_y;
+    int current_x, current_y, factor_x, factor_y,  euc_pixel, left_pixel, best_x, best_y;
+    int x=0, xt=0, x2=0, y=0, y2=0, euc_dist_min=-1, euc_index=0, euc_me=0;
 
-    //int i=feature_width+image_width*feature_height;
-    //int j=(image_width*image_height-1)-image_width*feature_height-feature_width;
     int i = 0;
 	while (i < image_height*image_width) {
-		////printf("entered");
-		//start comparing with other factors inside the box on the right image
 
 		current_x = i % (image_width);
 		current_y = (i-current_x)/image_width;
 		
-		//x+y*width=i
-		
-		//TODO maybe remove first half or OR?
 		if (current_x-maximum_displacement-feature_width < 0) {
 			//big green box sticks over left edge
 			xt=0;
@@ -98,19 +76,16 @@ void calc_depth(unsigned char *depth_map, unsigned char *left,
 
 		best_x=current_x;
 		best_y=current_y;
-		//printf("scanning features around pixel (%d,%d)\n", current_x, current_y);
-		//printf("ok big green box: x %d x2 %d y %d y2 %d\n", xt, x2, y, y2);
+
 		if (current_x<feature_width || current_y<feature_height || current_x>(image_width-1)-feature_width || current_y>(image_height-1)-feature_height){
-			////printf("yeah\n");
 			depth_map[i]=0;
 			i++;
 			continue;
 		}
-		//TODO just determine how far right and how far down the box needs to go from the padded x calculated up top. then no need for actually figuring out the size of the giant green box.
+
 		x=xt;
 		while (y <= y2){
 			while (x <= x2) {
-				//printf("scanning feature whose center is (%d,%d)\n", x+feature_width, y+feature_height);
 				//iterate through factor and contribute these values to the euclidian distance
 				factor_x = x;
 				factor_y = y;
@@ -119,21 +94,15 @@ void calc_depth(unsigned char *depth_map, unsigned char *left,
 					while (factor_x <= x+2*feature_width) {
 						euc_pixel = factor_x+factor_y*image_width;
 						left_pixel= ((current_x-feature_width)+(factor_x-x))+((current_y-feature_height)+(factor_y-y))*image_width;
-						//printf("left pixel=%d right pixel=%d\n", left_pixel, euc_pixel);
 						if (x+feature_width == current_x && y+feature_height == current_y){
-							//printf("modifying euc dist\n");
 							euc_me += (left[left_pixel]-right[euc_pixel])*(left[left_pixel]-right[euc_pixel]);
 						}
 						euc_index += (left[left_pixel]-right[euc_pixel])*(left[left_pixel]-right[euc_pixel]);
-
-						//printf("currently (%d,%d)\n", factor_x, factor_y);
-						//printf("feature width %d feature height %d\n", feature_width, feature_height);
 						factor_x++;
 					}
 					factor_y++;
 					factor_x=x; //reset factor_x
 				}
-				//printf("new euc calculated as %d\n", euc_index);
 				if (euc_index == euc_dist_min) {
 					if (normalized_displacement(best_x-current_x, best_y-current_y, maximum_displacement) > normalized_displacement((x+feature_width)-current_x, (y+feature_height)-current_y, maximum_displacement)){
 						best_x=(x+feature_width);
@@ -144,23 +113,18 @@ void calc_depth(unsigned char *depth_map, unsigned char *left,
 					euc_dist_min=euc_index;
 					best_x=x+feature_width;
 					best_y=y+feature_height;
-					//printf("bestx=%d besty=%d\n", best_x, best_y);
 				}
 				x++;
 				euc_index=0;
-				//euc_me=0;
 			}
 			y++;
 			x=xt; //reset x
 
 		}
 		if (euc_dist_min==euc_me) {
-			//printf("eucdist %d\n", euc_dist_min);
 			best_x=current_x;
 			best_y=current_y;
 		}
-		//printf("best match for left's x=%d y=%d is x=%d y=%d\n", current_x, current_y, best_x, best_y);
-		//printf("using parameters dx=%d dy=%d\n", best_x-current_x, best_y-current_y);
 		depth_map[i] = normalized_displacement(best_x-current_x, best_y-current_y, maximum_displacement);
 		i++;
 		euc_dist_min=-1;
@@ -168,7 +132,3 @@ void calc_depth(unsigned char *depth_map, unsigned char *left,
 	}
 }
 
-
-// function idea
-//returns array of x-displacements to form the feature box around on the right image
-// x is the current pixel's x value
